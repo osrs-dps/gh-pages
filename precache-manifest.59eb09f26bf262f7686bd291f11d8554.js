53977a1b7e05e81527045e8ac69f1a51be173839
Alex Jensen <ajensen@rednovalabs.com>
2020-04-02 14:10:51 -0600

    Updates

---
+++ precache-manifest.59eb09f26bf262f7686bd291f11d8554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d3b23cb7e4774371e66c4214f65c89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59487ee8fcbf674df596",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "14c529c74f975d84b571",
+    "url": "/static/css/main.d8574581.chunk.css"
+  },
+  {
+    "revision": "59487ee8fcbf674df596",
+    "url": "/static/js/2.86bb3c9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86bb3c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c529c74f975d84b571",
+    "url": "/static/js/main.efc8d154.chunk.js"
+  },
+  {
+    "revision": "30af27efcdb8cf333d78",
+    "url": "/static/js/runtime-main.efe8b9e6.js"
+  }
+]);
